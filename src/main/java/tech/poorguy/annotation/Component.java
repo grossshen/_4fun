a0e46c59e65b6f955e1ce0bc512f22a3c751b412
poorguy <494939649@qq.com>
2019-09-28 17:08:20 +0800

    [add] Add classes will be used

---
+++ src/main/java/tech/poorguy/annotation/Component.java
@@ -0,0 +1,8 @@
+package tech.poorguy.annotation;
+
+/**
+ * @Date 9/28/2019 3:52 PM
+ * @Mail 494939649@qq.com
+ **/
+public @interface Component {
+}
---
+++ src/main/java/tech/poorguy/bean/Book.java
@@ -0,0 +1,17 @@
+package tech.poorguy.bean;
+
+/**
+ * @Date 9/28/2019 4:28 PM
+ * @Mail 494939649@qq.com
+ **/
+public class Book {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
