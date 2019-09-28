cbe7ecf2863c856eee43555fc53864c663b5700b
poorguy <494939649@qq.com>
2019-09-28 15:51:09 +0800

    [add] init beans with xml files and then put them into contain. Then i will init beans with annotation

--- src/main/java/tech/poorguy/springLikeUtil/BeanContainer.java
+++ src/main/java/tech/poorguy/springLikeUtil/BeanContainer.java
@@ -1,8 +1,6 @@
 package tech.poorguy.springLikeUtil;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import tech.poorguy.domUtil.DOMUtil;
 import tech.poorguy.reflectUtil.ReflectUtil;
@@ -17,7 +15,7 @@ import java.util.Map;
 public class BeanContainer {
     public static Map<String,Object> beanMap=new HashMap<String, Object>();
 
-    public static void loadBeans(String path) {
+    public static void loadBeansWithXml(String path) {
         NodeList beanNodeList = DOMUtil.selectBeanNodeListFromBeansXml(path);
         for (int i = 0; i < beanNodeList.getLength(); i++) {
             Element item = (Element) beanNodeList.item(i);
@@ -25,11 +23,15 @@ public class BeanContainer {
         }
     }
 
+    public static void loadBeanWithAnnotation() {
+        
+    }
+
     public static Object getBean(String beanId) {
         return beanMap.get(beanId);
     }
     public static void main(String[] args){
-        loadBeans("C:\\work\\tqhy\\_4fun\\src\\main\\java\\Bean.xml");
+        loadBeansWithXml("C:\\work\\tqhy\\_4fun\\src\\main\\java\\Bean.xml");
         System.out.println(getBean("userMax"));
         System.out.println(getBean("userPg"));
     }
